cf5a333e1ebc2f00a12979d7f93f191130676889
Simon Stiebellehner <simon.stiebellehner@gmail.com>
2017-05-13 23:53:23 +0100

    version for 1 file added

--- cleaning_script.py
+++ cleaning_script.py
@@ -1,48 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-@author: stiebels
-"""
-
-###############################################################
-# Package Imports
-
 import pandas as pd
 
 
-###############################################################
-# Data Cleaning Functions
+class Converter(object):
+    
+    def __init__(self, path):
+        self._path = path
+        
+    @property
+    def path(self):
+        return self._path
+    
+    @path.setter
+    def path(self, p):
+        self._path = p
+        
+    def _load_file(self, path):
+        df = pd.read_csv(str(self._path), sep=" ", header=None)
+        return df
+        
+    def _drop_col(self, df):
+        '''
+        Arguments:
+            df: pandas dataframe object
+        Return:
+            df: original df with last column dropped (parsing leftover as data set as trailing space for each sample)
+        '''
+        #df.drop(df.columns[-1], axis=1, inplace=True)
+        return df.drop(df.columns[-1], axis=1)
+    
+    def _split_colon(self, df):
+        '''
+        Arguments:
+            df: pandas dataframe object
+        Return:
+            df: original df with string pattern ':' removed; columns named appropriately
+        '''
+        for col in range(1,len(df.columns)):
+            df.loc[:,col] = df.loc[:,col].apply(lambda x: str(x).split(':')[1])
+        df.columns = ['rel', 'qid'] + [str(x) for x in range(1,len(df.columns)-1)] # renaming cols
+        return df
+    
+    def convert(self):
+        df_raw = self._load_file(self._path)
+        df_drop = self._drop_col(df_raw)
+        return self._split_colon(df_drop)
+        
+    
+    
 
-# Dropping column with 100% nan values (parsing problem probably)
-def drop_col(df):
-    df.drop(df.columns[-1], axis=1, inplace=True)
-
-# Cleaning values
-def split_semicolon(df):
-    # removes string naming pattern '*:' from values
-    for col in range(1,len(df.columns)):
-        df.loc[:,col] = df.loc[:,col].apply(lambda x: str(x).split(':')[1])
-    df.columns = ['rel', 'qid'] + [str(x) for x in range(1,137)] # renaming cols
-
-
-###############################################################
-# Executing import & cleaning of files
-
-# Specify directory to your folder in which the 5 Fold folders are
-# Specify folds you want to clean (default: all)
-# Specify datasets you want to clean (detault: all)
-
-directory = '/your/path/to/parent/dir/of/Fold' # e.g. /home/user/Desktop/data/
-folds = [1,2,3,4,5]
-datasets = ['train', 'vali', 'test']
-
-paths = [directory+'Fold'+str(fold)+'/'+sets+'.txt' for fold in folds for sets in datasets]
-
-count = 0
-for path in paths:
-    count += 1
-    print(str(count)+'/'+str(len(paths)))
-    df = pd.read_csv(str(path), sep=" ", header = None)
-    drop_col(df)
-    split_semicolon(df)
-    df.to_csv(path[0:-4]+'_cleaned.csv') # saves in: directory/FoldX
-    del df # freeing up memory for next file
